e257906606b912aec95d629e60fe70780f24b435
Brian McClelland <47439126+bmcclelland@users.noreply.github.com>
2020-10-04 16:27:31 -0700

    Integrate templates

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def show_login():
-    # show login form and register link
-    # login form posts to action/login with parameters 'username' and 'password'
-    return 'login or register'
+    return render_template('index.html')
 
 @app.route('/action/login', methods=['POST'])
 def action_login():
     # try logging in with username and password from request
     # if success, redirect to /loggedin
     # if error, redirect back to /
-    return 'action/login'
+    return redirect('/loggedin')
 
 @app.route('/loggedin')
 def show_loggedin():
-    # show page for when user is logged in
-    return 'loggedin'
+    return render_template('home.html')
 
 @app.route('/register')
 def show_register():
-    # show register form
-    # form posts to action/register with parameters 'username' and 'password'
-    return 'register'
+    return render_template('register.html')
 
 @app.route('/action/register', methods=['POST'])
 def action_register():
     # try registering with username and password from request
     # if success, redirect to /
     # if error, redirect back to /register
-    return 'action/register'
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
