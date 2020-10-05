e174344a32abd8fce7b671fdbb51ca267c1c1fd3
Brian McClelland <47439126+bmcclelland@users.noreply.github.com>
2020-10-04 20:01:50 -0700

    Add error messages

--- main.py
+++ main.py
@@ -3,17 +3,28 @@ import backend
 
 app = Flask(__name__)
 
+def get_error_msg():
+    err = request.args.get('error')
+    if err is None:
+        return ""
+    elif err == 'bad_login':
+        return 'Login failed: invalid username/password'
+    elif err == 'name_in_use':
+        return 'Registration failed: username already in use'
+    else:
+        raise Exception('unknown error: ' + str(err))
+
 @app.route('/')
 def show_login():
-    return render_template('index.html')
+    msg = get_error_msg()
+    return render_template('index.html', msg=msg)
 
 @app.route('/action/login', methods=['POST'])
 def action_login():
     if backend.verify_user(request.form['username'], request.form['password']):
         return redirect('/loggedin')
     else:
-        # TODO error: invalid username/password
-        return redirect('/')
+        return redirect('/?error=bad_login')
 
 @app.route('/loggedin')
 def show_loggedin():
@@ -21,7 +32,8 @@ def show_loggedin():
 
 @app.route('/register')
 def show_register():
-    return render_template('register.html')
+    msg = get_error_msg()
+    return render_template('register.html', msg=msg)
 
 @app.route('/action/register', methods=['POST'])
 def action_register():
@@ -29,8 +41,7 @@ def action_register():
         backend.insert_user(request.form['username'], request.form['password'])
         return redirect('/')
     else:
-        # TODO error: username already exists
-        return redirect('/register')
+        return redirect('/register?error=name_in_use')
 
 if __name__ == '__main__':
     backend.create_user_table()
