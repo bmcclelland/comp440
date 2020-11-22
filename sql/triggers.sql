a53b407bf3f350c987ec6d9a7f3957d8354e167b
Brian McClelland <47439126+bmcclelland@users.noreply.github.com>
2020-11-21 19:57:20 -0800

    Reorder comment triggers by importance

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -11,14 +11,14 @@ CREATE TRIGGER CheckBlog BEFORE INSERT ON Blogs FOR EACH ROW BEGIN
 END;
 
 CREATE TRIGGER CheckComment BEFORE INSERT ON Comments FOR EACH ROW BEGIN
-    /* A user cannot post more than 3 comments per day. */
-    IF 3 <= (SELECT count(*) FROM Comments WHERE author=NEW.author AND commentdate=NEW.commentdate)
-        THEN SIGNAL SQLSTATE '99002';
+    /* A user cannot comment on their own blog. */
+    IF 0 < (SELECT count(*) FROM Blogs WHERE blogid=NEW.blogid AND author=NEW.author)
+        THEN SIGNAL SQLSTATE '99004';
     /* A user cannot post more than 1 comment per blog. */
     ELSEIF 1 <= (SELECT count(*) FROM Comments WHERE author=NEW.author AND blogid=NEW.blogid)
         THEN SIGNAL SQLSTATE '99003';
-    /* A user cannot comment on their own blog. */
-    ELSEIF 0 < (SELECT count(*) FROM Blogs WHERE blogid=NEW.blogid AND author=NEW.author)
-        THEN SIGNAL SQLSTATE '99004';
+    /* A user cannot post more than 3 comments per day. */
+    ELSEIF 3 <= (SELECT count(*) FROM Comments WHERE author=NEW.author AND commentdate=NEW.commentdate)
+        THEN SIGNAL SQLSTATE '99002';
     END IF;
 END;
